96e4ecb94d431d0ebd7b38d40b304d4a659e5611
gusyaaa <askerzadehuseyn2005@gmail.com>
2024-07-10 19:01:52 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function isValidNumber(phoneNumber) {
+
+    if (typeof phoneNumber !== 'string') {
+        return false;
+    }
+
+
+    if (phoneNumber.length === 11 && phoneNumber.startsWith('+7')) {
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
+console.log(isValidNumber("+79123456789")); 
+console.log(isValidNumber("79123456789"));  
+console.log(isValidNumber("+7123456789")); 
+console.log(isValidNumber("7123456789"));   
